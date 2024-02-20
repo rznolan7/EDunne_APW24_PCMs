8a8a85476680df0c6904311f320fda4cee7de298
Emma Dunne <39518460+emmadunne@users.noreply.github.com>
2024-02-20 12:42:05 +0100

    initial commit

---
+++ code/PCM_tutorial.R
@@ -0,0 +1,22 @@
+# ******************************************************
+#
+#   Introduction to Phylogenetic Comparative Methods
+#
+#   29.02.2024
+#
+#   Emma Dunne (emma.dunne@fau.de)
+#
+# ______________________________________________________
+#
+#   Tutorial 
+# 
+# ******************************************************
+
+
+## Load packages:
+library(tidyverse)
+library(ape)
+library(geiger)
+library(phytools)
+
+
